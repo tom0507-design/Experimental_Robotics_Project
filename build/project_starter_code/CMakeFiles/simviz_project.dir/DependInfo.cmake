3bbef38251c676cb57c57745fb6c12e04505270e
myubuntu <tom0507@stanford.edu>
2020-05-17 21:35:33 -0700

    First commit

---
+++ build/project_starter_code/CMakeFiles/simviz_project.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/myubuntu/sai2/apps/CS225A_Project/project_starter_code/simviz.cpp" "/home/myubuntu/sai2/apps/CS225A_Project/build/project_starter_code/CMakeFiles/simviz_project.dir/simviz.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "LINUX"
+  "USING_OTG"
+  "_LINUX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "/home/myubuntu/sai2/core/chai3d-master/src"
+  "/home/myubuntu/sai2/core/chai3d-master/external/glew/include"
+  "/home/myubuntu/sai2/core/sai2-simulation-master/src"
+  "/home/myubuntu/sai2/core/sai2-model-master/src"
+  "/home/myubuntu/sai2/core/sai2-model-master/rbdl/include"
+  "/home/myubuntu/sai2/core/sai2-model-master/rbdl/build/include"
+  "/home/myubuntu/sai2/core/sai2-graphics-master/src"
+  "/home/myubuntu/sai2/core/sai2-common-master/src"
+  "/usr/include/jsoncpp"
+  "/home/myubuntu/sai2/core/sai2-urdfreader-master/src"
+  "/home/myubuntu/sai2/core/sai2-urdfreader-master/src/urdf/urdfdom/urdf_parser/include"
+  "/home/myubuntu/sai2/core/sai2-primitives-master/src"
+  "/home/myubuntu/sai2/core/sai2-primitives-master/../external/ReflexxesTypeII/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
